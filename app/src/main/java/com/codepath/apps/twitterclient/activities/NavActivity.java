1ae81ebee140f78e3f45668131beed239b53e00b
Daniel Boeve <djboeve@gmail.com>
2015-05-30 14:25:17 -0700

    Adds shell activity for viewing your profile

--- app/src/main/java/com/codepath/apps/twitterclient/activities/NavActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/activities/NavActivity.java
@@ -1,8 +1,11 @@
 package com.codepath.apps.twitterclient.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.codepath.apps.twitterclient.R;
@@ -19,5 +22,16 @@ public class NavActivity extends ActionBarActivity {
 
     PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
     tabsStrip.setViewPager(viewPager);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_nav, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  public void onProfile(MenuItem item) {
+    Intent i = new Intent(this, ProfileActivity.class);
+    startActivity(i);
   }
 }
